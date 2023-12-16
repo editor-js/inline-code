67dac7b22f4ee4479415838f5ccc2afc2a281eea
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2023-12-16 13:06:40 +0500

    added example files

---
+++ example/example_data.js
@@ -0,0 +1,18 @@
+let example_data = {
+  blocks: [
+    {
+      "id": "HhK9RKb2Qz",
+      "type": "paragraph",
+      "data": {
+        "text": "My <code class=\"inline-code\">inline code</code>"
+      }
+    },
+    {
+      "id": "w2bEmRrvOC",
+      "type": "paragraph",
+      "data": {
+        "text": "My <code class=\"inline-code\">inline</code> code"
+      }
+    }
+  ],
+};
